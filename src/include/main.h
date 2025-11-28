54309f5fa89ed7bf5a6b6c3261e895847b1f695b
guapi-exe <3051016381@qq.com>
2025-11-28 11:56:34 +0800

    test

--- src/include/main.h
+++ src/include/main.h
@@ -5,7 +5,7 @@
 #define CAMERA_HEIGHT   2160
 #define IMAGE_WIDTH     1280
 #define IMAGE_HEIGHT    720
-#define CAMERA_INDEX_1  22
+#define CAMERA_INDEX_1  23
 #define CAMERA_INDEX_2  51
 #define IMGRATIO        3
 #define IMAGE_SIZE      (CAMERA_WIDTH*CAMERA_HEIGHT*IMGRATIO)
