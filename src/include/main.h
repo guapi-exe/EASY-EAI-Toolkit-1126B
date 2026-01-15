797354c47ed360b926655a2c999cf70f6c07a4d3
guapi-exe <3051016381@qq.com>
2026-01-15 18:48:16 +0800

    更新

--- src/include/main.h
+++ src/include/main.h
@@ -3,8 +3,8 @@
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 #define CAMERA_WIDTH    2688
 #define CAMERA_HEIGHT   1520
-#define IMAGE_WIDTH     1280
-#define IMAGE_HEIGHT    720
+#define IMAGE_WIDTH     640
+#define IMAGE_HEIGHT    640
 #define CAMERA_INDEX_1  23
 #define CAMERA_INDEX_2  51
 #define IMGRATIO        3
