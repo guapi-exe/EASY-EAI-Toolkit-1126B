09ec824291cc08a5e8f42bf584bb04b1aedfc627
guapi-exe <3051016381@qq.com>
2026-03-27 10:00:20 +0800

    test

--- src/utils/device_config.cpp
+++ src/utils/device_config.cpp
@@ -9,6 +9,35 @@
 using nlohmann::json;
 
 namespace {
+static bool patchMissingFields(json& target, const json& defaults) {
+    bool changed = false;
+
+    if (!defaults.is_object()) {
+        return false;
+    }
+    if (!target.is_object()) {
+        target = json::object();
+        changed = true;
+    }
+
+    for (auto it = defaults.begin(); it != defaults.end(); ++it) {
+        const std::string& key = it.key();
+        const json& defaultValue = it.value();
+
+        if (!target.contains(key)) {
+            target[key] = defaultValue;
+            changed = true;
+            continue;
+        }
+
+        if (defaultValue.is_object()) {
+            changed = patchMissingFields(target[key], defaultValue) || changed;
+        }
+    }
+
+    return changed;
+}
+
 static json buildDefaultJson(const DeviceConfig& cfg) {
     json j;
     j["device"] = {
@@ -28,7 +57,10 @@ static json buildDefaultJson(const DeviceConfig& cfg) {
     };
     j["capture_defaults"] = {
         {"min_clarity", CAPTURE_MIN_CLARITY},
+        {"fallback_min_clarity", CAPTURE_FALLBACK_MIN_CLARITY},
         {"max_motion_ratio", CAPTURE_MAX_MOTION_RATIO},
+        {"max_motion_reject_ratio", CAPTURE_MAX_MOTION_REJECT_RATIO},
+        {"person_detect_interval", CAPTURE_PERSON_DETECT_INTERVAL},
         {"face_detect_interval", CAPTURE_FACE_DETECT_INTERVAL},
         {"face_input_max_width", CAPTURE_FACE_INPUT_MAX_WIDTH},
         {"min_face_score", CAPTURE_MIN_FACE_SCORE},
@@ -36,15 +68,37 @@ static json buildDefaultJson(const DeviceConfig& cfg) {
         {"headshot_down_shift", CAPTURE_HEADSHOT_DOWN_SHIFT},
         {"focus_scale_factor", CAPTURE_FOCUS_SCALE_FACTOR},
         {"min_area_ratio", CAPTURE_MIN_AREA_RATIO},
+        {"near_area_ratio", CAPTURE_NEAR_AREA_RATIO},
+        {"area_score_target_ratio", CAPTURE_AREA_SCORE_TARGET_RATIO},
         {"approach_ratio_pos", CAPTURE_APPROACH_RATIO_POS},
         {"approach_ratio_neg", CAPTURE_APPROACH_RATIO_NEG},
         {"min_track_hits", CAPTURE_MIN_TRACK_HITS},
         {"require_approach", CAPTURE_REQUIRE_APPROACH},
+        {"require_frontal_face", CAPTURE_REQUIRE_FRONTAL_FACE},
         {"max_yaw", CAPTURE_MAX_YAW},
+        {"fallback_max_yaw", CAPTURE_FALLBACK_MAX_YAW},
+        {"strong_frontal_max_roll", CAPTURE_STRONG_FRONTAL_MAX_ROLL},
+        {"strong_frontal_max_yaw", CAPTURE_STRONG_FRONTAL_MAX_YAW},
+        {"min_face_box_short_side", CAPTURE_MIN_FACE_BOX_SHORT_SIDE},
+        {"min_face_box_area", CAPTURE_MIN_FACE_BOX_AREA},
+        {"max_person_occlusion", CAPTURE_MAX_PERSON_OCCLUSION},
+        {"max_face_edge_occlusion", CAPTURE_MAX_FACE_EDGE_OCCLUSION},
+        {"fallback_max_face_edge_occlusion", CAPTURE_FALLBACK_MAX_FACE_EDGE_OCCLUSION},
+        {"max_blur_severity", CAPTURE_MAX_BLUR_SEVERITY},
+        {"fallback_max_blur_severity", CAPTURE_FALLBACK_MAX_BLUR_SEVERITY},
         {"brightness_black_threshold", cfg.brightnessBlackThreshold}
     };
     j["ircut"] = {
         {"brightness_black_threshold", cfg.brightnessBlackThreshold}
+    };
+    j["brightness_boost"] = {
+        {"target", CAMERA_BRIGHTNESS_TARGET},
+        {"boost_threshold", CAMERA_BRIGHTNESS_BOOST_THRESHOLD},
+        {"boost_min_floor", CAMERA_BRIGHTNESS_BOOST_MIN_FLOOR},
+        {"max_alpha", CAMERA_BRIGHTNESS_MAX_ALPHA},
+        {"max_beta", CAMERA_BRIGHTNESS_MAX_BETA},
+        {"gamma", CAMERA_BRIGHTNESS_GAMMA},
+        {"dark_blend", CAMERA_BRIGHTNESS_DARK_BLEND}
     };
     return j;
 }
@@ -128,6 +182,16 @@ bool DeviceConfig::loadOrCreate(const std::string& filePath) {
         json j;
         ifs >> j;
         loadFromJson(this, j);
+
+        json normalized = buildDefaultJson(*this);
+        bool patched = patchMissingFields(j, normalized);
+        if (patched) {
+            std::ofstream ofs(filePath, std::ios::out | std::ios::trunc);
+            if (!ofs.is_open()) {
+                return false;
+            }
+            ofs << j.dump(4);
+        }
     } catch (...) {
         return false;
     }
